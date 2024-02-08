43cc9474f472a40c0cf7ca57a7e899ab06a5cd6d
assaffmatias <matias.assaff98@gmail.com>
2024-02-08 12:31:26 -0300

    Landig

--- src/views/Landing/Landing.jsx
+++ src/views/Landing/Landing.jsx
@@ -10,55 +10,36 @@ import video from '../../assets/fondo.mp4'
 const Landing = () => {
     return (
         <div className={style.container}>
-            <div className={style.div_video}>
-                <video muted autoPlay loop className={style.video}>
-                    <source src={video} type='video/mp4' />
-                </video>
-            </div>
-            <div className={style.content}>
-                <img src={logo} alt="" className={style.logo} />
-                <h2 className={style.h2}>DRIVERS API</h2>
-                <div className={style.divButtons}>
-                    <Link to={PATHROUTES.DRIVERS}>
-                        <button className={style.button1}>GET INTO</button>
-                    </Link>
-                    <Link to={PATHROUTES.ABOUT}>
-                        <button className={style.button}>ABOUT</button>
-                    </Link>
+            <div className={style.container2}>
+                <div className={style.div_video}>
+                    <video muted autoPlay loop className={style.video}>
+                        <source src={video} type='video/mp4' />
+                    </video>
+                </div>
+                <div className={style.content}>
+                    <img src={logo} alt="" className={style.logo} />
+                    <h2 className={style.h2}>DRIVERS API</h2>
+                    <div className={style.divButtons}>
+                        <a href="#wellcome">
+                            <button className={style.button1}>WELCOME</button>
+                        </a>
+                        <Link to={PATHROUTES.ABOUT}>
+                            <button className={style.button}>ABOUT ME</button>
+                        </Link>
+                    </div>
                 </div>
             </div>
             <div className={style.line}>1</div>
-            <div>
-                <footer className={style.footer}>
+            <div className={style.div_wellcome}>
+                <h2 id='wellcome' className={style.wellcome}>WELLCOME</h2>
+                <p className={style.p}>Welcome to the F1® Drivers API! Explore the world of Formula 1 racing by accessing detailed information about your favorite drivers. Whether you're a seasoned fan or new to the sport, our API provides data on drivers and more. Start exploring now and enjoy the journey through the fast-paced world of F1® racing!</p>
+                <button className={style.button3}>START NOW</button>
+                {/* <footer className={style.footer}>
                     <img src={porsche} alt="" className={style.teams} />
                     <img src={mclaren} alt="" className={style.teams} />
                     <img src={redbull} alt="" className={style.teams} />
-                </footer>
+                </footer> */}
             </div>
-            {/* <div className={style.content}>
-                <video muted autoPlay loop className={style.video}>
-                    <source src={video} type='video/mp4' />
-                </video>
-                <img src={logo} alt="" className={style.logo} />
-                <h2 className={style.h2}>DRIVERS API</h2>
-                <div className={style.divButtons}>
-                    <Link to={PATHROUTES.DRIVERS}>
-                        <button className={style.button1}>GET INTO</button>
-                    </Link>
-                    <Link to={PATHROUTES.ABOUT}>
-                        <button className={style.button}>ABOUT</button>
-                    </Link>
-                </div>
-            </div>
-            <div className={style.line}>1</div>
-            <div>
-                <footer className={style.footer}>
-                    <img src={porsche} alt="" className={style.teams} />
-                    <img src={mclaren} alt="" className={style.teams} />
-                    <img src={redbull} alt="" className={style.teams} />
-                </footer>
-            </div> */}
-
         </div >
     )
 }
