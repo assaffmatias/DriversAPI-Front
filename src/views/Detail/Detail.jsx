1f976fa135cb19fe6fdee398867bb8cb2c5f93d7
assaffmatias <matias.assaff@gmail.com>
2024-04-29 07:43:24 -0300

    update

--- src/views/Detail/Detail.jsx
+++ src/views/Detail/Detail.jsx
@@ -51,7 +51,7 @@ const Detail = () => {
                                     <div className={style.imgContainer}>
                                         <img className={style.img} src={driver.image} alt="" />
                                         <button className={style.closeButton} onClick={handleGoBack}>
-                                            <svg className={style.svg} aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
+                                            <svg className={style.svg} aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="34" height="34" fill="currentColor" viewBox="0 0 24 24">
                                                 <path fill-rule="evenodd" d="M2 12C2 6.477 6.477 2 12 2s10 4.477 10 10-4.477 10-10 10S2 17.523 2 12Zm7.707-3.707a1 1 0 0 0-1.414 1.414L10.586 12l-2.293 2.293a1 1 0 1 0 1.414 1.414L12 13.414l2.293 2.293a1 1 0 0 0 1.414-1.414L13.414 12l2.293-2.293a1 1 0 0 0-1.414-1.414L12 10.586 9.707 8.293Z" clip-rule="evenodd" />
                                             </svg>
                                         </button>
@@ -108,7 +108,7 @@ const Detail = () => {
                                     <div className={style.imgContainer}>
                                         <img className={style.img} src={driver.image} alt="" />
                                         <button className={style.closeButton} onClick={handleGoBack}>
-                                            <svg className={style.svg} aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="34" height="34" fill="currentColor" viewBox="0 0 24 24">
+                                            <svg className={style.svg} aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="44" height="44" fill="currentColor" viewBox="0 0 24 24">
                                                 <path fill-rule="evenodd" d="M2 12C2 6.477 6.477 2 12 2s10 4.477 10 10-4.477 10-10 10S2 17.523 2 12Zm7.707-3.707a1 1 0 0 0-1.414 1.414L10.586 12l-2.293 2.293a1 1 0 1 0 1.414 1.414L12 13.414l2.293 2.293a1 1 0 0 0 1.414-1.414L13.414 12l2.293-2.293a1 1 0 0 0-1.414-1.414L12 10.586 9.707 8.293Z" clip-rule="evenodd" />
                                             </svg>
                                         </button>
